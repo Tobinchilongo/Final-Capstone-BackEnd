76b69db191c8fe1b71ccb9f4da59aeb34a35c35b
Tobinchilongo <tobinchilongojnr@gmail.com>
2022-07-07 17:25:07 +0200

    Generated `Car` Model

---
+++ db/migrate/20220707152130_create_cars.rb
@@ -0,0 +1,14 @@
+class CreateCars < ActiveRecord::Migration[7.0]
+  def change
+    create_table :cars do |t|
+      t.string :name
+      t.text :description
+      t.string :image_url
+      t.string :city
+      t.integer :user_id
+      t.integer :price
+
+      t.timestamps
+    end
+  end
+end
