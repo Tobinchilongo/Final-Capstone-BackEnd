e8ab04ff34766edf9458a1cd558bdd48499cba20
Jerry Owusu <jerryowusu6133@gmail.com>
2022-07-08 18:15:01 -0400

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1, defaults: { format: 'json' } do
+      get 'users', to: 'users#index'
       resources :cars, only: [:index, :create, :show, :destroy]
       resources :reservation, only: [:index,:show, :create ,:new,:destroy, :update]
       resources :users, only: [:index, :create]
