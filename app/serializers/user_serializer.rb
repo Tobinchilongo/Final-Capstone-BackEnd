b6b2ec888447a69937673ee888cbe801ae5e3d3b
Jerry Owusu <jerryowusu6133@gmail.com>
2022-07-09 13:27:07 -0400

    Update user data list

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :created_at, :updated_at
+  attributes :id, :name, :email
   has_many :cars
   has_many :reservations
 end
