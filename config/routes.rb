e1f147643ead8c6d356725a899d918ea99cdea27
Jerry Owusu <jerryowusu6133@gmail.com>
2022-07-15 16:56:30 -0400

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
+
+  
   namespace :api do
     namespace :v1, defaults: { format: 'json' } do
       resources :cars, only: [:index, :create, :show, :destroy]
