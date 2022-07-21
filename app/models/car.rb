aa98f73de9fb217746e707010de42356f3d54e4e
Tobinchilongo <tobinchilongojnr@gmail.com>
2022-07-21 09:58:29 +0200

    Added Description to the cars table

--- app/models/car.rb
+++ app/models/car.rb
@@ -6,5 +6,6 @@ class Car < ApplicationRecord
   validates :model, presence: true, length: { minimum: 3, maximum: 100 }
   validates :price, presence: true
   validates :image_url, presence: true
+  validates :description, presence: true
   validates :user_id, presence: true
 end
