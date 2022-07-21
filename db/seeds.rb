b5e33810c2eeaa1fa24f19e8245d3d2e117cd202
Jerry Owusu <jerryowusu6133@gmail.com>
2022-07-21 13:51:26 -0400

    Update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,9 @@ user1 = User.create(name: 'Jerry Owusu', email: 'jerry@gmail.com', password: 'je
 user2 = User.create(name: 'Tobin Chilongo', email: 'tobin@gmail.com', password: 'tobinchilongo')
 user3 = User.create(name: 'Candy Inga', email: 'candy@gmail.com', password: 'candyinga')
 
-car1 = Car.create(name: 'Jeep', model: 'Jeep Wrangler', price: 2000, user_id: user1.id, image_url: 'https://user-images.githubusercontent.com/65233860/178718381-cd6cfcff-a804-4d35-95af-705794d4c4a8.png')
-car2 = Car.create(name: 'Hyndai', model: 'Santa Fe', price: 1400, user_id: user2.id, image_url: 'https://user-images.githubusercontent.com/65233860/178718933-f55aea72-e56c-4f14-9032-a4902203f77a.png')
-car3 = Car.create(name: 'Ford', model: 'Ford Mustang', price: 1350, user_id: user3.id, image_url: 'https://user-images.githubusercontent.com/65233860/178718657-5d0b9f51-8a97-4045-a14a-c70913891275.png')
+car1 = Car.create(name: 'Jeep', model: 'Jeep Wrangler', description: 'One of our best cars', price: 2000, user_id: user1.id, image_url: 'https://user-images.githubusercontent.com/65233860/178718381-cd6cfcff-a804-4d35-95af-705794d4c4a8.png')
+car2 = Car.create(name: 'Hyndai', model: 'Santa Fe', description: 'One of our best cars', price: 1400, user_id: user2.id, image_url: 'https://user-images.githubusercontent.com/65233860/178718933-f55aea72-e56c-4f14-9032-a4902203f77a.png')
+car3 = Car.create(name: 'Ford', model: 'Ford Mustang', description: 'One of our best cars', price: 1350, user_id: user3.id, image_url: 'https://user-images.githubusercontent.com/65233860/178718657-5d0b9f51-8a97-4045-a14a-c70913891275.png')
 
 reservation1 = Reservation.create(date: '2018-01-01', city: 'New York', user_id: user1.id, car_id: car1.id)
 reservation2 = Reservation.create(date: '2018-01-01', city: 'Zambia', user_id: user2.id, car_id: car2.id)
